a3c0151b7532f3461dd330a496e59b6531f0b0f1
anirudh-K442 <79716755+anirudh-K442@users.noreply.github.com>
2022-01-16 12:10:36 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+function btn1_go_to()
+{
+    window.location = "btn1.html";
+}
+
+function btn2_go_to()
+{
+    window.location = "btn2.html";
+}
+
+function btn3_go_to()
+{
+    window.location = "btn3.html";
+}
+
+function btn4_go_to()
+{
+    window.location = "btn4.html";
+}
+
+function btn5_go_to()
+{
+    window.location = "btn5.html";
+}
+
+function btn6_go_to()
+{
+    window.location = "btn6.html";
+}
+
+function btn7_go_to()
+{
+    window.location = "btn7.html";
+}
+
+function btn9_go_to()
+{
+    window.location = "btn9.html";
+}
+
+function btn10_go_to()
+{
+    window.location = "btn10.html";
+}
+
+function btn11_go_to()
+{
+    window.location = "btn11.html";
+}
+
+function btn12_go_to()
+{
+    window.location = "btn12.html";
+}
